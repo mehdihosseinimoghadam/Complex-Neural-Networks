c067a6f151352eca01e18a7927f9b5bbbfc6f94b
Mehdi Hosseini Mohadam <m.h.moghadam1996@gmail.com>
2022-02-12 00:24:38 +0330

    Update complex_neural_net.py

--- complex_neural_net.py
+++ complex_neural_net.py
@@ -1,6 +1,10 @@
 from torch import nn
 import torch
 
+
+
+##______________________________________Complex Convolution 2d_____________________________________________
+
 class CConv2d(nn.Module):
   def __init__(self, in_channels, out_channels, **kwargs):
     super(CConv2d, self).__init__()
@@ -24,3 +28,35 @@ class CConv2d(nn.Module):
     out = torch.cat([out_re, out_im], -1) 
 
     return out
+
+  
+  ##___________________________________Complex Convolution Transpose 2d_______________________________________________
+  
+  
+  class CConvTrans2d(nn.Module):
+  def __init__(self, in_channels, out_channels, **kwargs):
+    super(CConvTrans2d, self).__init__()
+    self.in_channels = in_channels
+    self.out_channels = out_channels
+
+
+    self.re_Tconv = nn.ConvTranspose2d(self.in_channels, self.out_channels, **kwargs)
+    self.im_Tconv = nn.ConvTranspose2d(self.in_channels, self.out_channels, **kwargs)
+
+    nn.init.xavier_uniform_(self.re_Tconv.weight)
+    nn.init.xavier_uniform_(self.im_Tconv.weight)
+
+
+  def forward(self, x):  
+    x_re = x[..., 0]
+    x_im = x[..., 1]
+
+    out_re = self.re_Tconv(x_re) - self.im_Tconv(x_im)
+    out_im = self.re_Tconv(x_im) + self.im_Tconv(x_re)
+
+    out = torch.cat([out_re, out_im], -1) 
+
+    return out
+  
+  
+  
